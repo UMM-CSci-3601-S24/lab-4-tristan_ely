b80921877c0c659699d36b847b856b4c390de9f5
Ely Johnson <elyjohnson6@gmail.com>
2024-02-13 14:13:08 -0600

    Created the Todo class with equals and hashCode() methods
    Co-authored-by: Tristan Kalvoda <kalvo007@morris.umn.edu>

--- server/src/main/java/umm3601/todo/Todo.java
+++ server/src/main/java/umm3601/todo/Todo.java
@@ -1,5 +1,31 @@
 package umm3601.todo;
 
-public class Todo {
+import org.mongojack.Id;
+import org.mongojack.ObjectId;
 
-}
+@SuppressWarnings({"VisibilityModifier"})
+  public class Todo {
+
+    @ObjectId @Id
+    @SuppressWarnings({"MemberName"})
+    public String _id;
+
+    public String owner;
+    public String body;
+    public String category;
+    public boolean status;
+
+    @Override
+    public boolean equals(Object obj) {
+      if (!(obj instanceof Todo)) {
+        return false;
+      }
+      Todo other = (Todo) obj;
+      return _id.equals(other._id);
+    }
+
+    @Override
+    public int hashCode() {
+      return _id.hashCode();
+    }
+  }
