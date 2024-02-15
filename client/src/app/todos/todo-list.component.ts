e445e956014f6f8aacd4aaa80c104ed6e35b105e
Ely Johnson <elyjohnson6@gmail.com>
2024-02-15 17:31:41 -0600

    Fixed some list component stuff by copying from previous lab
    Co-authored-by: Tristan Kalvoda <kalvo007@morris.umn.edu>

--- client/src/app/todos/todo-list.component.ts
+++ client/src/app/todos/todo-list.component.ts
@@ -4,8 +4,8 @@ import { Todo } from './todo';
 import { TodoService } from './todo.service';
 import { Subject, takeUntil } from 'rxjs';
 import { RouterLink } from '@angular/router';
-import { MatNavList, MatListSubheaderCssMatStyler, MatListItem, MatListItemTitle, MatListItemLine } from '@angular/material/list';
-import { TodoCardComponent} from './todo-card.component';
+import { MatNavList, MatListSubheaderCssMatStyler, MatListItem, MatListItemAvatar, MatListItemTitle, MatListItemLine } from '@angular/material/list';
+
 import { MatRadioGroup, MatRadioButton } from '@angular/material/radio';
 import { MatOption } from '@angular/material/core';
 import { MatSelect } from '@angular/material/select';
@@ -15,54 +15,45 @@ import { MatFormField, MatLabel, MatHint, MatError } from '@angular/material/for
 import { MatCard, MatCardTitle, MatCardContent } from '@angular/material/card';
 
 
-/**
- * A component that displays a list of todos, either as a grid
- * of cards or as a vertical list.
- *
- * The component supports local filtering by Owner and/or Body,
- * and remote filtering (i.e., filtering by the server) by
- * Status and/or Category.
- */
-
 @Component({
-    selector: 'app-todo-list-component',
-    templateUrl: 'todo-list.component.html',
-    styleUrls: ['./todo-list.component.scss'],
-    providers: [],
-    standalone: true,
-    imports: [TodoCardComponent, MatCard, MatCardTitle, MatCardContent, MatFormField, MatLabel, MatInput, FormsModule, MatHint, MatSelect, MatOption, MatRadioGroup, MatRadioButton, MatNavList, MatListSubheaderCssMatStyler, MatListItem, RouterLink, MatListItemTitle, MatListItemLine, MatError]
+  selector: 'app-todo-list-component',
+  templateUrl: 'todo-list.component.html',
+  styleUrl: './todo-list.component.scss',
+  providers: [],
+  standalone: true,
+  imports: [MatCard, MatCardTitle, MatCardContent, MatFormField, MatLabel, MatInput, FormsModule, MatHint, MatSelect, MatOption, MatRadioGroup, MatRadioButton, MatNavList, MatListSubheaderCssMatStyler, MatListItem, RouterLink, MatListItemAvatar, MatListItemTitle, MatListItemLine, MatError],
 })
-export class TodoListComponent implements OnInit, OnDestroy {
-  // These are public so that tests can reference them (.spec.ts)
-  public serverFilteredTodos: Todo[];
-  public filteredTodos: Todo[];
 
-  public todoOwner: string;
-  public todoStatus: boolean;
-  public todoCategory: string;
-  public todoBody: string;
-  public todoLimit: number;
-  public viewType: 'card' | 'list' = 'card';
+export class TodoListComponent implements OnInit, OnDestroy{
+public serverFilteredTodos: Todo[];
+public filteredTodos: Todo[];
 
-  errMsg = '';
-  private ngUnsubscribe = new Subject<void>();
+public todoLimit: number;
+public todoStatus: boolean;
+public todoOwner: string;
+public todoBody: string;
+public todoCategory: string;
+public todoOrderBy: string;
+
+errMsg='';
+private ngUnsubscribe = new Subject<void>();
 
   /**
-   * This constructor injects both an instance of `TodoService`
+   * This constructor injects both an instance of `UserService`
    * and an instance of `MatSnackBar` into this component.
    *
-   * @param todoService the `TodoService` used to get todos from the server
+   * @param todoService the `UserService` used to get users from the server
    * @param snackBar the `MatSnackBar` used to display feedback
    */
   constructor(private todoService: TodoService, private snackBar: MatSnackBar) {
-    // Nothing here – everything is in the injection parameters.
   }
 
-  /**
-   * Get the todos from the server, filtered by the parameters set in the GUI
-   */
-  getTodosFromServer() {
+
+  getTodosFromServer(): void {
+
     this.todoService.getTodos({
+      orderBy: this.todoOrderBy,
+      body: this.todoBody,
       category: this.todoCategory
     }).pipe(
       takeUntil(this.ngUnsubscribe)
@@ -70,24 +61,20 @@ export class TodoListComponent implements OnInit, OnDestroy {
       next: (returnedTodos) => {
         this.serverFilteredTodos = returnedTodos;
         this.updateFilter();
-        },
+      },
       error: (err) => {
         if (err.error instanceof ErrorEvent) {
           this.errMsg = `Problem in the client – Error: ${err.error.message}`;
         } else {
           this.errMsg = `Problem contacting the server – Error Code: ${err.status}\nMessage: ${err.message}`;
         }
-      }
+      },
     })
   }
 
   public updateFilter() {
     this.filteredTodos = this.todoService.filterTodos(
-      this.serverFilteredTodos, { owner: this.todoOwner,
-                                  status: this.todoStatus,
-                                  category: this.todoCategory,
-                                  body: this.todoBody,
-                                  limit: this.todoLimit }
+      this.serverFilteredTodos, { limit: this.todoLimit, status: this.todoStatus, owner: this.todoOwner }
     );
   }
 
@@ -95,7 +82,7 @@ export class TodoListComponent implements OnInit, OnDestroy {
     this.getTodosFromServer();
   }
 
-  ngOnDestroy(): void {
+  ngOnDestroy() {
     this.ngUnsubscribe.next();
     this.ngUnsubscribe.complete();
   }
--- client/src/app/todos/todo.service.ts
+++ client/src/app/todos/todo.service.ts
@@ -8,77 +8,46 @@ import { Todo } from './todo';
   providedIn: 'root'
 })
 export class TodoService {
-  // The URL for the todos part of the server API.
   readonly todoUrl: string = environment.apiUrl + 'todos';
 
-  // The private `HttpClient` is *injected* into the service
-  // More information in user.service.ts
   constructor(private httpClient: HttpClient) {
   }
-
-  /**
-   * Get all the todos from the server, filtered by the information
-   * in the `filters` map.
-   *
-   * @param filters a map that allows us to specify a target title, description, or category to filter by, or any combination of those
-   * @returns an `Observable` of an array of `Todos`.
-   */
-  getTodos(filters?: { owner?: string; status?: boolean; body?: string; category?: string; limit?: number}): Observable<Todo[]> {
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+  getTodos(filters?: { body?: string; category?: string; orderBy?: string }): Observable<Todo[]> {
     let httpParams: HttpParams = new HttpParams();
     if (filters) {
-      if (filters.owner) {
-        httpParams = httpParams.set('owner', filters.owner);
-      }
-      if (filters.status) {
-        httpParams = httpParams.set('status', filters.status);
-      }
       if (filters.body) {
-        httpParams = httpParams.set('body', filters.body);
+        httpParams = httpParams.set('contains', filters.body);
       }
       if (filters.category) {
         httpParams = httpParams.set('category', filters.category);
       }
-      if (filters.limit) {
-        httpParams = httpParams.set('limit', filters.limit);
+      if (filters.orderBy) {
+        httpParams = httpParams.set('orderBy', filters.orderBy);
       }
-
     }
+
     return this.httpClient.get<Todo[]>(this.todoUrl, {
       params: httpParams,
-    });
+    })
   }
-/**
- * @param id The ID of the Todo to get
- * @returns an `Observable` of the `Todo` with the given ID
- */
-  getTodoById(id: string): Observable<Todo> {
-    return this.httpClient.get<Todo>(this.todoUrl + '/' + id);
-  }
-
-  filterTodos(todos: Todo[], filters: { owner?: string; status?: boolean; body?: string; category?: string; limit?: number}): Todo[] {
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+  filterTodos(todos: Todo[], filters: {limit?: number, status?: boolean, owner?: string } ): Todo[] {
     let filteredTodos = todos;
-    if (filters) {
-      if (filters.owner) {
-        filters.owner = filters.owner.toLowerCase();
-        filteredTodos = filteredTodos.filter(todo => todo.owner.toLowerCase().indexOf(filters.owner) !== -1);
-      }
-      // make sure the status filter is working!
-      if (filters.status !== undefined) {
-        filteredTodos = filteredTodos.filter(todo => todo.status === filters.status);
-      }
-      if (filters.body) {
-        filters.body = filters.body.toLowerCase();
-        filteredTodos = filteredTodos.filter(todo => todo.body.toLowerCase().indexOf(filters.body) !== -1);
-      }
-      if (filters.category) {
-        filters.category = filters.category.toLowerCase();
-        filteredTodos = filteredTodos.filter(todo => todo.category.indexOf(filters.category) !== -1);
-      }
-      if (filters.limit) {
-        filteredTodos = filteredTodos.slice(0, filters.limit);
-      }
 
+    if(filters.status != null) {
+      filteredTodos = filteredTodos.filter(todo => todo.status === filters.status)
     }
+
+    if(filters.owner) {
+      filters.owner = filters.owner.toLowerCase();
+      filteredTodos = filteredTodos.filter(todo => todo.owner.toLowerCase().indexOf(filters.owner) !== -1);
+    }
+    if (filters.limit) {
+      filteredTodos = filteredTodos.slice(0, filters.limit);
+    }
+
     return filteredTodos;
   }
+
 }
