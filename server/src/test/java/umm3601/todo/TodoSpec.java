a8d2a52d1a6b4d611453be6f56a30ac8f91b452a
Ely Johnson <elyjohnson6@gmail.com>
2024-02-15 12:59:12 -0600

    Set up TodoSpec.java test variables and BeforeEach
    Co-authored-by: Tristan Kalvoda <TristanKalvoda@users.noreply.github.com>

--- server/src/test/java/umm3601/todo/TodoSpec.java
+++ server/src/test/java/umm3601/todo/TodoSpec.java
@@ -1,5 +1,18 @@
 package umm3601.todo;
 
+import org.junit.jupiter.api.BeforeEach;
+
 public class TodoSpec {
 
+private static final String FAKE_ID_STRING_1 = "fakeIdOne";
+private static final String FAKE_ID_STRING_2 = "fakeIdTwo";
+
+private Todo todo1;
+private Todo todo2;
+
+@BeforeEach
+  void setupEach() {
+    todo1 = new Todo();
+    todo2 = new Todo();
+  }
 }
