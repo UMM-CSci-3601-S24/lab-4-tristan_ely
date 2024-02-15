3c239c82908482c09729fb5956458a4613a00388
Ely Johnson <elyjohnson6@gmail.com>
2024-02-15 13:06:39 -0600

    Added tests for todos ids equal and different ids are not equal
    Co-authored-by: Tristan Kalvoda <kalvo007@morris.umn.edu>

--- server/src/test/java/umm3601/todo/TodoSpec.java
+++ server/src/test/java/umm3601/todo/TodoSpec.java
@@ -1,6 +1,10 @@
 package umm3601.todo;
 
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 
 public class TodoSpec {
 
@@ -10,9 +14,25 @@ private static final String FAKE_ID_STRING_2 = "fakeIdTwo";
 private Todo todo1;
 private Todo todo2;
 
-@BeforeEach
-  void setupEach() {
-    todo1 = new Todo();
-    todo2 = new Todo();
+  @BeforeEach
+    void setupEach() {
+      todo1 = new Todo();
+      todo2 = new Todo();
+    }
+
+  @Test
+  void todosWithEqualIdAreEqual() {
+    todo1._id = FAKE_ID_STRING_1;
+    todo2._id = FAKE_ID_STRING_1;
+
+    assertTrue(todo1._id.equals(todo2._id));
+  }
+
+
+  @Test
+  void todosWithDifferentIdAreNotEqual() {
+    todo1._id = FAKE_ID_STRING_1;
+    todo2._id = FAKE_ID_STRING_2;
+    assertFalse(todo1.equals(todo2));
   }
 }
