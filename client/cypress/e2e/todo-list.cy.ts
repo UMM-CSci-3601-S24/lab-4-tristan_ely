f754af00a8f628caff5e0c2b2b791082109693b5
Ely Johnson <elyjohnson6@gmail.com>
2024-02-15 17:33:42 -0600

    Added e2e tests but e2e testing isn't working so minimum tests
    Co-authored-by: Tristan Kalvoda <kalvo007@morris.umn.edu>

---
+++ client/cypress/e2e/todo-list.cy.ts
@@ -0,0 +1,19 @@
+import { TodoListPage } from "cypress/support/todo-list.po";
+
+const page = new TodoListPage();
+
+describe('Todo list', () => {
+
+  beforeEach(() => {
+    page.navigateTo();
+  });
+
+  it('Should have the correct page title', () => {
+    page.getPageTitle().should('eq', 'Todos');
+  });
+
+  it('Should have the correct title', () => {
+    page.getTodoTitle().should('have.text', 'Todos');
+  });
+});
+
